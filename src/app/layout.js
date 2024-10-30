588e993c0aa1280f5ef8d42b4b3764b3fc847bca
Jawad Ahmed <qwertykey41@gmail.com>
2024-10-30 21:01:53 +0500

    layout update

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,8 +34,8 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
         >
           <Navbar/>
-          <Footer/>
           {children}
+          <Footer/>
         </ThemeProvider>
       </body>
     </html>
